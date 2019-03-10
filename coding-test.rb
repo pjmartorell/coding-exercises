21259f07b936f2797e43e4ed8376371c0a3cde99
Pere Joan Martorell <pj.martorell@gmail.com>
2019-03-10 05:51:34 +0100

    Add exercise 1

--- coding-test.rb
+++ coding-test.rb
@@ -1,5 +1,31 @@
 module CodingTest
     class << self
+        # Exercise 1
+        # Given an array where every element occurs three times, except one element which occurs only once. 
+        # Find the element that occurs once.
+        def find_unique_1(array)
+            times = Hash.new(0)
+            for i in 0..array.length-1 do
+                times[array[i]] += 1
+            end
+            times.key(1)
+        end
+
+        def find_unique_2(array)
+          sorted_array = array.sort
+          three_arrays = Array.new(3,0)
+          sorted_array.each_with_index do |elem, index|
+              current = index % 3
+              three_arrays[current] = elem + three_arrays[current]
+          end
+          three_arrays[0] - three_arrays[1]
+        end
+
+        def find_unique_3(array)
+            uniques = array.uniq
+            (3 * uniques.sum - array.sum) / 2
+        end
+
         # Exercise 2
         # Given two numbers a and b. Write a program to count number of bits needed to be flipped to
         # convert a to b.
--- spec/coding_test_spec.rb
+++ spec/coding_test_spec.rb
@@ -2,6 +2,28 @@ require 'spec_helper'
 require_relative '../coding-test'
 
 describe CodingTest do
+
+    describe ".find_unique_1" do
+        it "should find the element that occurs once" do
+            array =  [12, 1, 12, 3, 12, 1, 1, 2, 3, 3]
+            expect(subject.find_unique_1(array)).to eq(2)
+        end
+    end
+
+    describe ".find_unique_2" do
+        it "should find the element that occurs once" do
+            array = [12, 1, 12, 3, 12, 1, 1, 2, 3, 3]
+            expect(subject.find_unique_2(array)).to eq(2)
+        end
+    end
+
+    describe ".find_unique_3" do
+        it "should find the element that occurs once" do
+            array = [12, 1, 12, 3, 12, 1, 1, 2, 3, 3]
+            expect(subject.find_unique_3(array)).to eq(2)
+        end
+    end
+
     describe ".count_flipped_bits" do
         it "should count number of bits to be flipped to convert A to B" do
             a = 7
