46ce4ecf4fe69795872345429dad63dbd3132ef6
Pere Joan Martorell <pj.martorell@gmail.com>
2019-03-10 18:58:22 +0100

    Add exercises 4, 5 and 6

--- coding-test.rb
+++ coding-test.rb
@@ -39,14 +39,63 @@ module CodingTest
         # given sum value
         def count_triplets(array, sum)
             count = 0
-            for i in 0..array.length-1 do
-                for j in i+1..array.length-1 do
-                    for k in j+1..array.length-1 do
+            n = array.length-1
+            for i in 0..n do
+                for j in i+1..n do
+                    for k in j+1..n do
                         count += 1 if array[i] + array[j] + array[k] < sum
                     end
                 end
             end
             count
+        end
+
+        # Exercise 4
+        # Given an array of distinct integers, find length of the longest subarray which contains numbers that
+        # can be arranged in a continuous sequence.
+        def longest_continous_subarray(array)
+            n = array.length-1
+            max_length = 1
+            for i in 0..n do
+                min = max = array[i]
+                for j in i+1..n do
+                    min = [min, array[j]].min
+                    max = [max, array[j]].max
+                    length = max - min
+                    if ((j-i) == length)
+                        max_length = [max_length, length + 1].max
+                    end
+                end
+            end
+            max_length
+        end
+
+        # Exercise 5
+        # Given a set of integers, the task is to divide it into two sets S1 and S2 such that the absolute
+        # difference between their sums is minimum.
+        def minimum_diff_subsets(array)
+            minimum_diff_subsets_rec(array, array.length-1, 0, 0)
+        end
+
+        def minimum_diff_subsets_rec(array, n, subset1, subset2)
+            if (n < 0)
+                return (subset1 - subset2).abs
+            end
+            diff1 = minimum_diff_subsets_rec(array, n-1, subset1 + array[n], subset2)
+            diff2 = minimum_diff_subsets_rec(array, n-1, subset1, subset2 + array[n])
+            [diff1, diff2].min
+        end
+
+        # Exercise 6
+        # Given two array of strings, the first with phrases, the second with words, we need to know what is
+        # the longest phrase that could be formed with a collection of words.
+        # NOTE: I assume words can be repeated and they are case-sensitive. In case of valid phrases with equal
+        # length, the first is returned.
+        def longest_phrase(phrases, words)
+            phrases.select do |phrase|
+                phrase_words = phrase.gsub(/([.,;])/, ' \1 ').split
+                phrase_words.all? {|phrase_word| words.include? phrase_word}
+            end.max_by {|phrase| phrase.length}.to_s
         end
     end
 end
--- spec/coding_test_spec.rb
+++ spec/coding_test_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 require_relative '../coding-test'
 
 describe CodingTest do
-
     describe ".find_unique_1" do
         it "should find the element that occurs once" do
             array =  [12, 1, 12, 3, 12, 1, 1, 2, 3, 3]
@@ -34,10 +33,47 @@ describe CodingTest do
 
     describe ".count_triplets" do
 		it "should count triplets with sum smaller than a given valuei" do
-			array = [5, 1, 3, 4, 7]
-			sum = 12
-			expect(subject.count_triplets(array, sum)).to eq(4)
+            array = [5, 1, 3, 4, 7]
+            sum = 12
+            expect(subject.count_triplets(array, sum)).to eq(4)
 		end
-	end
-end
+    end
 
+    describe ".longest_continous_subarray" do
+        it "should find the longest continous subarray" do
+            array = [1, 56, 58, 57, 90, 92, 94, 93, 91, 45]
+            expect(subject.longest_continous_subarray(array)).to eq(5)
+        end
+    end
+
+    describe ".minimum_diff_subsets" do
+        it "should find the minimum difference between sums of 2 subsets" do
+            array = [1, 6, 11, 5]
+            expect(subject.minimum_diff_subsets(array)).to eq(1)
+        end
+    end
+
+    describe ".longest_phrase" do
+        context "when there are phrases formed with the collection of words" do
+            it "should find the longest phrase formed with a collection of words" do
+                words =  ["kiss", "why", "me", "did", "you", "elbow", ","]
+                phrases = ["kiss me, elbow", "why did you kiss me"]
+                expect(subject.longest_phrase(phrases, words)).to eq("why did you kiss me")
+            end
+
+            it "should find the longest phrase formed with a collection of words" do
+                words = ["kiss", "why", "me", "did", "you", "elbow", "kiss-me", "a-", ",", ".", ";", "C3PO_"]
+                phrases = [".a-.kiss-me.a-,me..elbow;me C3PO_", "why did you kiss me, why did you.", "123456789012345678901234567890123"]
+                expect(subject.longest_phrase(phrases, words)).to eq(".a-.kiss-me.a-,me..elbow;me C3PO_")
+            end
+        end
+
+        context "when there are no phrase formed with the collection of words" do
+            it "should return an empty string" do
+                words = ['kiss', 'me', 'did', 'you', ',']
+                phrases = ['kiss me, elbow', 'why did you kiss me']
+                expect(subject.longest_phrase(phrases, words)).to eq("")
+            end
+        end
+    end
+end
